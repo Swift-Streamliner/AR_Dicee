1aa70535f1b4870f461beffbc6d6f9583add8b7e
thisisstreamliner <thisisstreamliner@gmail.com>
2021-06-02 10:35:42 +0300

    Import Dice Model and Texture.  Add to Scene.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -23,24 +23,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //sceneView.showsStatistics = true
         
         //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        let sphere = SCNSphere(radius: 0.2)
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/2k_moon.jpeg")
-        sphere.materials = [material]
+        //let sphere = SCNSphere(radius: 0.2)
+        //let material = SCNMaterial()
+        //material.diffuse.contents = UIImage(named: "art.scnassets/2k_moon.jpeg")
+        //sphere.materials = [material]
         
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        node.geometry = sphere
-        sceneView.scene.rootNode.addChildNode(node)
+        //let node = SCNNode()
+        //node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        //node.geometry = sphere
+        //sceneView.scene.rootNode.addChildNode(node)
+        showDice()
         sceneView.autoenablesDefaultLighting = true
     }
     
-    private func showAirShip() {
+    private func showDice() {
         // Create a new scene
-        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        guard let diceNode = scene.rootNode.childNode(withName: "Dice", recursively: true) else { return }
+        diceNode.position = SCNVector3(x:0, y:0, z: -0.1)
+        
         
         // Set the scene to the view
-        //sceneView.scene = scene
+        sceneView.scene.rootNode.addChildNode(diceNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
