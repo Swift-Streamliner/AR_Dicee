13717a38d69d5a581e647089e80554f1e7bae5b9
thisisstreamliner <thisisstreamliner@gmail.com>
2021-06-15 12:05:58 +0300

    Roll all dice at once

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,6 +10,8 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    var diceArray = [SCNNode]()
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -20,21 +22,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        //sceneView.showsStatistics = true
-        
-        //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        //let sphere = SCNSphere(radius: 0.2)
-        //let material = SCNMaterial()
-        //material.diffuse.contents = UIImage(named: "art.scnassets/2k_moon.jpeg")
-        //sphere.materials = [material]
-        
-        //let node = SCNNode()
-        //node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        //node.geometry = sphere
-        //sceneView.scene.rootNode.addChildNode(node)
-        
         sceneView.autoenablesDefaultLighting = true
     }
     
@@ -47,12 +34,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             y: position.y + diceNode.boundingSphere.radius,
             z: position.z)
         
-        
+        diceArray.append(diceNode)
         // Set the scene to the view
         sceneView.scene.rootNode.addChildNode(diceNode)
+    }
+    
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode) {
         let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
         let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
-        diceNode.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 5), y: 0, z: CGFloat(randomZ * 5), duration: 0.5))
+        dice.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 5), y: 0, z: CGFloat(randomZ * 5), duration: 0.5))
+    }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
     }
     
     override func viewWillAppear(_ animated: Bool) {
